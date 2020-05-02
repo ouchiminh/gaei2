2350c7f394224cbfbc93ca4c99dcdbb918d747bc
ouchiminh <39828763+ouchiminh@users.noreply.github.com>
2020-05-03 00:43:32 +0900

    Update Sensor.cs

--- 2d/Assets/Scripts/Navigation/Sensor.cs
+++ 2d/Assets/Scripts/Navigation/Sensor.cs
@@ -16,7 +16,14 @@ namespace gaei.navi
         /// <param name="position">観測した障害物が存在する小空間</param>
         /// <param name="sensor">イベントの送信者</param>
         /// <param name="velocity">観測した障害物の絶対的な移動ベクトル</param>
-        public delegate void ObstacleEvent(Area position, Sensor sensor, UnityEngine.Vector3 velocity);
+        public delegate void ScanEvent(Area position, Sensor sensor, UnityEngine.Vector3 velocity);
+
+        // これまで発見していた障害物を見失った際に発行される
+        public ScanEvent onLostObstacle;
+        // 未発見或いは見失っていた障害物を捕捉した際に発行される
+        public ScanEvent onCaptureObstacle;
+
+        public void scan() { throw new NotImplementedException(); }
 
         /// <summary>
         /// 指定された方向をスキャンし、障害物の有無を確かめる。
