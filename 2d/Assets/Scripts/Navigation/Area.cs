ef0bcfecaef4cf3515d041c54a096f7d5c081962
ouchiminh <39828763+ouchiminh@users.noreply.github.com>
2020-05-03 00:37:23 +0900

    added sensor & area

---
+++ 2d/Assets/Scripts/Navigation/Area.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UnityEngine;
+
+namespace gaei.navi
+{
+    // 世界を分割する小空間(立方体)
+    public struct Area
+    {
+        public Area(Vector3Int pos) { representativePoint = pos; }
+        // 小空間の一辺の長さ
+        public static readonly int size = 1;
+        // 小空間の代表点(小空間の中で最も原点に近い点
+        public readonly Vector3Int representativePoint;
+
+        // 小空間の各頂点
+        public Vector3Int[] vertexesInt { get {
+                Vector3Int[] res = new Vector3Int[8];
+                for (int i = 0; i < 8; ++i)
+                {
+                    res[i] = representativePoint + new Vector3Int((i & 1) == 1 ? size : 0, (i & 2) == 2 ? size : 0, (i & 4) == 4 ? size : 0);
+                }
+                return res;
+            }
+        }
+        public Vector3[] vertexes { get {
+                var res = new Vector3[8];
+                var resi = vertexesInt;
+                for (int i = 0; i < 8; ++i)
+                {
+                    res[i] = resi[i];
+                }
+                return res;
+            }
+        }
+    }
+}
---
+++ 2d/Assets/Scripts/Navigation/Sensor.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+
+namespace gaei.navi
+{
+    public class Sensor
+    {
+        public enum scanResult
+        {
+            somethingFound, nothingFound, unobservable
+        }
+
+        /// <summary>
+        /// 障害物を観測した際にトリガーされるイベントのハンドラの型
+        /// </summary>
+        /// <param name="position">観測した障害物が存在する小空間</param>
+        /// <param name="sensor">イベントの送信者</param>
+        /// <param name="velocity">観測した障害物の絶対的な移動ベクトル</param>
+        public delegate void ObstacleEvent(Area position, Sensor sensor, UnityEngine.Vector3 velocity);
+
+        /// <summary>
+        /// 指定された方向をスキャンし、障害物の有無を確かめる。
+        /// </summary>
+        /// <param name="direction">方向</param>
+        /// <param name="position">障害物があればその位置。なければ値は保証されない。</param>
+        /// <param name="velocity">障害物があればその移動ベクトル。なければ値は保証されない。</param>
+        /// <exception cref="NullReferenceException">velocityかpositionにnullが指定されていて、かつ 障害物が見つかった場合</exception>
+        public scanResult lookd(UnityEngine.Vector3 direction, ref Area position, ref UnityEngine.Vector3 velocity) { throw new NotImplementedException(); }
+        /// <summary>
+        /// 指定された小空間をスキャンし、障害物の有無を確かめる。
+        /// </summary>
+        /// <param name="area">見たい小空間</param>
+        /// <param name="velocity">障害物が存在していればその移動ベクトル。なければ値は保証されない。</param>
+        public scanResult looka(Area area, ref UnityEngine.Vector3 velocity) { throw new NotImplementedException(); }
+    }
+}
+
