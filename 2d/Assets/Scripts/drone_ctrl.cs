13e1cd50a9e933c98822aef5d49b622d619d9c6a
kongou923 <39829359+kongou923@users.noreply.github.com>
2020-05-04 00:48:52 +0900

    Update drone_ctrl

--- 2d/Assets/Scripts/drone_ctrl.cs
+++ 2d/Assets/Scripts/drone_ctrl.cs
@@ -2,17 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class drone_ctrl : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(-1f, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(1f, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(0, 1f, 0);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(0, -1f, 0);
+        }
     }
 }
