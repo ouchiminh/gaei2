7e52045343e0bd255c0ce776e5322358169f6ce4
ouchiminh <39828763+ouchiminh@users.noreply.github.com>
2020-05-05 19:33:00 +0900

    add CAM class

---
+++ 2d/Assets/Scripts/Navigation/CAM.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace gaei.navi
+{
+    public class CAM : PathProposer
+    {
+        public override void onCaptureObstacle(in Sensor s, Area pos, Vector3 velocity)
+        {
+
+        }
+
+        public override void onLostObstacle(in Sensor s, Area pos, Vector3 velocity)
+        {
+        }
+    }
+}
+
