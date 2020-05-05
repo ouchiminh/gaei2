70b6496da27dcfcfa2d080fc84e35da043700387
kongou923 <39829359+kongou923@users.noreply.github.com>
2020-05-05 15:24:54 +0900

    change Obstacle and DroneCtrl name

--- 2d/Assets/ObstacleCtrl.cs
+++ 2d/Assets/ObstacleCtrl.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class obstacle_cntr : MonoBehaviour
+public class ObstacleCtrl : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
--- 2d/Assets/Scripts/DroneCtrl.cs
+++ 2d/Assets/Scripts/DroneCtrl.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class drone_ctrl : MonoBehaviour
+public class DroneCtrl : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
