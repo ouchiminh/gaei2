d47ab401a5478b41eb9259036fd4365d579cacad
sp-mixer <44938483+sp-mixer@users.noreply.github.com>
2020-05-20 23:48:55 +0900

    randomwalk_block

--- 3D/Assets/ObstacleCtrl.cs
+++ 3D/Assets/ObstacleCtrl.cs
@@ -4,17 +4,34 @@ using UnityEngine;
 
 public class ObstacleCtrl : MonoBehaviour
 {
+    public float speed = 1f;
+    public float rotationspeed = 1f;
+    public float posrange = 10f;
+    private Vector3 targetpos;
+    private float changetarget = 50f;
+    private float targetdistance;
+    Vector3 GetRandomPosition(Vector3 currentpos){
+        return new Vector3(Random.Range(-posrange + currentpos.x, posrange + currentpos.x), 0, Random.Range(-posrange + currentpos.z, posrange + currentpos.z));
+    }
 
+    void automove(){
+        if (targetdistance < changetarget) targetpos = GetRandomPosition(transform.position);
+        Quaternion targetRotation = Quaternion.LookRotation(targetpos - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * rotationspeed);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        Debug.Log("automove");
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        targetpos = GetRandomPosition(transform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        targetdistance = Vector3.SqrMagnitude(transform.position - targetpos);
+        automove();
     }
     void OnCollisionEnter(Collision collision){
        Debug.Log(collision.gameObject.name);
