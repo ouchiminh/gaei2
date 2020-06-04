4f090bcf4a92e6c47677cbcba77b0830f7a7412a
ouchiminh <39828763+ouchiminh@users.noreply.github.com>
2020-06-04 11:16:05 +0900

    update

---
+++ 3D/Assets/Scripts/Navigation/Fuhrer.cs
@@ -0,0 +1,14 @@
+﻿using gaei.navi;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace gaei.navi
+{
+    public class Fuhrer
+    {
+        public void addDemandPoint(Area area) { }
+        public void addSupplyPoint(Area area) { }
+        public void updateDroneState() { }
+    }
+}
