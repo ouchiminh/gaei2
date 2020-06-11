8534962cdaba0b445e5a42a39afc3401c4bb1a13
ouchiminh <39828763+ouchiminh@users.noreply.github.com>
2020-06-11 11:50:21 +0900

    fix

--- 3D/Assets/Scripts/Navigation/PathProposer.cs
+++ 3D/Assets/Scripts/Navigation/PathProposer.cs
@@ -6,11 +6,11 @@ using UnityEngine;
 namespace gaei.navi {
     public interface GlobalPathProposer
     {
-        public IEnumerable<Area> getPath(Vector3 dest, Vector3 here, in Dictionary<Area, (Sensor.ScanResult accessibility, Vector3? velocity)> envmap);
+        IEnumerable<Area> getPath(Vector3 dest, Vector3 here, in Dictionary<Area, (Sensor.ScanResult accessibility, Vector3? velocity)> envmap);
     }
 
     public interface LocalPathProposer
     {
-        public Vector3 getCourse(Vector3 dest, Vector3 here, in Dictionary<Area, (Sensor.ScanResult accessibility, Vector3? velocity)> envmap);
+        Vector3 getCourse(Vector3 dest, Vector3 here, in Dictionary<Area, (Sensor.ScanResult accessibility, Vector3? velocity)> envmap);
     }
 }
