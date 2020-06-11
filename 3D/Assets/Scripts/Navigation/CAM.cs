539dab49eae38e521bf14c330706d3bfb0571fef
ouchiminh <39828763+ouchiminh@users.noreply.github.com>
2020-06-11 11:57:06 +0900

    Update CAM.cs

--- 3D/Assets/Scripts/Navigation/CAM.cs
+++ 3D/Assets/Scripts/Navigation/CAM.cs
@@ -6,7 +6,7 @@ namespace gaei.navi
 {
     public class CAM : LocalPathProposer
     {
-        public Vector3 getCourse(Vector3 dest, Vector3 here, Dictionary<Area, (Sensor.ScanResult accessibility, Vector3? velocity)> envmap)
+        public Vector3 getCourse(Vector3 dest, Vector3 here, in Dictionary<Area, (Sensor.ScanResult accessibility, Vector3? velocity)> envmap)
         {
             throw new System.NotImplementedException();
         }
