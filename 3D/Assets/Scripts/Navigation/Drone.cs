92dcd2ccdccaef12439439253aaf8a02fda14de0
ouchiminh <39828763+ouchiminh@users.noreply.github.com>
2020-06-21 12:32:34 +0900

    update

--- 3D/Assets/Scripts/Navigation/Drone.cs
+++ 3D/Assets/Scripts/Navigation/Drone.cs
@@ -10,22 +10,29 @@ namespace gaei.navi
 {
     public class DroneCtrl : MonoBehaviour
     {
+        // 最大の速さ (area/seconds)
+        const float maxSpeed = 1;
+        const float sqrMaxSpeed = maxSpeed * maxSpeed;
         public enum Status
         {
             idle, delivery, homing
         }
         public Status status { get => (Status)(status_ %= 3); set { status_ = (uint)value; } }
         public uint status_;
+        public Vector3 velocity { get; private set; }
         Navigator navi_;
         Fuhrer fuhrer_;
 
         public void initialize(Fuhrer fuhrer) { fuhrer_ = fuhrer; }
         void Update()
         {
-            gameObject.transform.Translate(Time.deltaTime * navi_.getNextCourse(Sensor.envmap));
+            gameObject.transform.Translate(velocity*Time.deltaTime);
         }
         private void FixedUpdate()
         {
+            var velbuf = navi_.getNextCourse(Sensor.envmap);
+            velocity = velbuf.sqrMagnitude < sqrMaxSpeed ? velbuf : velbuf.normalized * maxSpeed;
+
             if (navi_.remainingWayPointCount == 0)
             {
                 fuhrer_.updateDroneState(this);
